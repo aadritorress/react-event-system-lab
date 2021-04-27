44ed865c094d7642913269f7da6602bbd1e5c4ed
Adriana <adriana.amaral@msn.com>
2021-04-27 18:12:24 -0500

    commit tree e8c2eebeedd5f7fa9b97ba9a2ec37c0200c59220

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+
+class EyesOnMe extends React.Component {
+
+render()
+{
+    return(
+     <button onClickCapture={console.log('Good!')} </button>)
+}
+
+}
+
+
+export default EyesOnMe
